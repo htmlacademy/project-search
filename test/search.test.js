0ce0dae3079862649c850d47562e8a2da09376ea
Alexey Maleykov <a@htmlacademy.ru>
2017-02-02 17:11:59 +0300

    Fix tests

--- test/search.test.js
+++ test/search.test.js
@@ -7,23 +7,49 @@ describe(`Search for files and types`, () => {
   const testDir = path.join(__dirname, `data`);
 
   describe(`Find existing files`, () => {
-    it(`All html files`, () => search.html(testDir).should.be.fulfilledWith([`catalog.html`, `index.html`]));
+    it(`All html files`, () => {
+      return search.html(testDir).should.be.fulfilledWith(
+        [`catalog.html`, `index.html`]
+      );
+    });
 
-    it(`All js files`, () => search.js(testDir).should.be.fulfilledWith([`js/some.js`]));
+    it(`All js files`, () => {
+      return search.js(testDir).should.be.fulfilledWith(
+        [`js/some.js`]
+      );
+    });
 
-    it(`All css files`, () => search.css(testDir).should.be.fulfilledWith([`css/normalise.min.css`, `css/style.css`]));
+    it(`All css files`, () => {
+      return search.css(testDir).should.be.fulfilledWith(
+        [`css/normalise.min.css`, `css/style.css`]
+      );
+    });
 
-    it(`All images`, () => search.images(testDir).should.be.fulfilledWith([`img/1.png`, `img/2.svg`, `img/3.jpg`, `img/4.gif`, `img/5.bmp`]));
+    it(`All images`, () => {
+      return search.images(testDir).should.be.fulfilledWith(
+        [`img/1.png`, `img/2.svg`, `img/3.jpg`, `img/4.gif`, `img/5.bmp`]
+      );
+    });
 
-    it(`All files by extension`, () => search.type(testDir, `html`).should.be.fulfilledWith([`catalog.html`, `index.html`]));
+    it(`All files by extension`, () => {
+      return search.type(testDir, `html`).should.be.fulfilledWith(
+        [`catalog.html`, `index.html`]
+      );
+    });
 
-    it(`.hidden file`, () => search.file(testDir, `.hidden`).should.be.fulfilledWith(`.hidden`));
+    it(`.hidden file`, () => {
+      return search.file(testDir, `.hidden`).should.be.fulfilledWith(`.hidden`);
+    });
   });
 
   describe(`Find not existenting files`, () => {
-    it(`Txt files`, () => search.type(testDir, `txt`).should.be.fulfilledWith([]));
+    it(`Txt files`, () => {
+      return search.type(testDir, `txt`).should.be.fulfilledWith([]);
+    });
 
-    it(`Doc file`, () => search.file(testDir, `doc`).should.be.fulfilledWith(null));
+    it(`Doc file`, () => {
+      return search.file(testDir, `doc`).should.be.fulfilledWith(null);
+    });
   });
 
 });
