2babe8646f1331a7f95dadb371edc4894591b037
Alexey Maleykov <a@htmlacademy.ru>
2017-02-02 16:44:38 +0300

    Add more tests

--- test/search.test.js
+++ test/search.test.js
@@ -9,13 +9,21 @@ describe(`Search for files and types`, () => {
   describe(`Find existing files`, () => {
     it(`All html files`, () => search.html(testDir).should.be.fulfilledWith([`catalog.html`, `index.html`]));
 
-    it(`All js file`, () => search.js(testDir).should.be.fulfilledWith([`js/some.js`]));
+    it(`All js files`, () => search.js(testDir).should.be.fulfilledWith([`js/some.js`]));
+
+    it(`All css files`, () => search.css(testDir).should.be.fulfilledWith([`css/normalise.min.css`, `css/style.css`]));
+
+    it(`All images`, () => search.images(testDir).should.be.fulfilledWith([`img/1.png`, `img/2.svg`, `img/3.jpg`, `img/4.gif`, `img/5.bmp`]));
+
+    it(`All files by extension`, () => search.type(testDir, `html`).should.be.fulfilledWith([`catalog.html`, `index.html`]));
 
     it(`.hidden file`, () => search.file(testDir, `.hidden`).should.be.fulfilledWith(`.hidden`));
+  });
 
-    it(`All images`, () => search.images(testDir).should.be.fulfilledWith(
-      [`img/1.png`, `img/2.svg`, `img/3.jpg`, `img/4.gif`, `img/5.bmp`]
-    ));
+  describe(`Find not existenting files`, () => {
+    it(`Txt files`, () => search.type(testDir, `txt`).should.be.fulfilledWith([]));
+
+    it(`Doc file`, () => search.file(testDir, `doc`).should.be.fulfilledWith(null));
   });
 
 });
